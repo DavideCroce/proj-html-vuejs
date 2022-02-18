55a805c4f5466f7bf15aa549459bbb65bb0c4baf
IskahnX <davidecroce10@gmail.com>
2022-02-18 14:45:57 +0100

    first settings added

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="header">
+      
+  </div>
+</template>
+
+<script>
+export default {
+name:"Header",
+components:{
+
+}
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
