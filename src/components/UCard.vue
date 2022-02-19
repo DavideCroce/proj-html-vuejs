7185f86a72306c341cb3f4c3816b458c02bdb1e1
IskahnX <davidecroce10@gmail.com>
2022-02-19 14:24:14 +0100

    dynamics blog updates card added

--- src/components/UCard.vue
+++ src/components/UCard.vue
@@ -1,37 +1,49 @@
 <template>
   <div class="update-card container">
           <div class="row">
-            <div class="col-3">
-              <img src="../assets/img/motivation-blog-04-480x325.jpg" alt="">
-              <div class="date"><i class="far fa-calendar"></i> May 13, 2020</div>
-              <h3>How to Stay True to Your Personal Brand</h3>
-              <p class="description">When it comes to your busines or career, you want ...</p>
-            </div>
-              <div class="col-3">
-              <img src="../assets/img/motivation-blog-03-480x325.jpg" alt="">
-              <div class="date"><i class="far fa-calendar"></i> May 13, 2020</div>
-              <h3>5 Vital Lessons in 5 Years of Freelancing</h3>
-              <p class="description">Being self-emplyes and working form home it's easy to get ...</p>
-            </div>
-              <div class="col-3">
-              <img src="../assets/img/motivation-blog-02-480x325.jpg" alt="">
-              <div class="date"><i class="far fa-calendar"></i> May 13, 2020</div>
-              <h3>11 Super Useful Tips for Small-Business Owners</h3>
-              <p class="description">Being a small-business owner poses a ton of challenges. We ...</p>
-            </div>
-              <div class="col-3">
-              <img src="../assets/img/motivation-blog-01-480x325.jpg" alt="">
-              <div class="date"><i class="far fa-calendar"></i> May 13, 2020</div>
-              <h3>How to Give Yourself Grace to Start Again</h3>
-              <p class="description">Forgive yourself for bad habits you may have started or ...</p>
-            </div>
+            <div v-for="update in updates" :key="update.lenght" class="col-3">
+              <img :src="require(`../assets/img/motivation-blog-${update.image}-480x325.jpg`)" alt="">
+              <div class="date"><i class="far fa-calendar"></i> {{update.date}}</div>
+              <h3>{{update.title}}</h3>
+              <p class="description">{{update.description}} ...</p>
           </div>
+        </div>
         </div>
 </template>
 
 <script>
 export default {
 name: "UCard",
+data(){
+  return{
+    updates: [
+      {
+        image: '04',
+        date: 'May 13, 2020',
+        title: 'How to Stay True to Your Personal Brand',
+        description: 'When it comes to your busines or career, you want',
+      },
+      {
+        image: '03',
+        date: 'May 13, 2020',
+        title: '5 Vital Lessons in 5 Years of Freelancing',
+        description: "Being self-emplyes and working form home it's easy to get",
+      },
+      {
+        image: '02',
+        date: 'May 13, 2020',
+        title: '11 Super Useful Tips for Small-Business Owners',
+        description: 'Being a small-business owner poses a ton of challenges. We',
+      },
+      {
+        image: '01',
+        date: 'May 13, 2020',
+        title: 'How to Give Yourself Grace to Start Again',
+        description: 'Forgive yourself for bad habits you may have started or',
+      },
+    ]
+  }
+}
 }
 </script>
 
