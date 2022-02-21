89e34c0daa4b1adffd6f28f15aa957b3512ec287
IskahnX <davidecroce10@gmail.com>
2022-02-21 11:46:12 +0100

    reactive about me video added

--- src/components/AboutMeVideo.vue
+++ src/components/AboutMeVideo.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="aboutme-video-player container">
           <div class="row">
-              <div class="col-8 d-flex justify-content-center">
+              <div class="col-12 col-sm-12 col-md-8 d-flex justify-content-center align-items-center video-player">
                   <img class="yt-button" src="../assets/img/icon-youtube-play.png" alt="">
               </div>
-              <div class="col-4 align-items-center">
+              <div class="col-12 col-sm-12 col-md-4 align-items-center description">
                   <h2>"Every morning you have two choices: continue to sleep with your dreams, or wake up and chase them".</h2>
                   <img src="../assets/img/home-movation-shape-01.png" alt="">
                   <p class="name"><strong>Kaixa Stark</strong></p>
@@ -28,17 +28,17 @@ export default {
 .aboutme-video-player{
     padding-bottom: 150px;
 }
-.col-8{
+.video-player{
     background-image: url(../assets/img/home-movation-video-poster-670x450.jpg);
     background-size: cover;
     background-repeat: no-repeat;
     border-radius: 10px;
-    padding: 250px 0;
+    // padding: 250px 0;
     img{
         width: 20%;
     }
 }
-.col-4{
+.description{
     height: 100%;
     padding: 60px;
     h2{
@@ -56,6 +56,24 @@ export default {
     .small{
         font-size: 12px;
         color: grey;
+    }
+}
+@media screen and (max-width: 575px){
+    .video-player{
+        height: 450px;
+        background-position: center;
+        img{
+            height: 17%;
+        }
+    }
+}
+@media screen and (max-width: 767px){
+    .video-player{
+        background-position: center;
+        height: 450px;
+        img{
+            height: 17%;
+        }
     }
 }
 </style>
