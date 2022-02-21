e709c57f68552e05190ca32d68356723da431308
IskahnX <davidecroce10@gmail.com>
2022-02-21 11:32:36 +0100

    reactive main/stats added

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="stats text-center container">
       <div class="row">
-        <div class="col-3">
+        <div class="col-3 bord">
         <h1>1.926</h1>
         <p>FINISHED SESSION</p>
       </div>
-      <div class="col-3">
+      <div class="col-3 bord">
         <h1>100%</h1>
         <p>SATISFACTION RATE</p>
       </div>
-      <div class="col-3">
+      <div class="col-3 bord">
         <h1>3092+</h1>
         <p>ENROLLED LEARNERS</p>
       </div>
-      <div class="col-3 endborder">
+      <div class="col-3 bord endborder">
         <h1>200</h1>
         <p>ONLINE INSTRUCTORS</p>
       </div>
@@ -42,5 +42,20 @@ name:"Stats",
 .stats{
   padding: 200px 0;
   font-family: 'Josefin Sans', sans-serif;
+}
+@media screen and (max-width: 575px){
+  .bord{
+    border-top: lightblue dashed 1px;
+    border-left: none;
+    padding: 10px 0;
+  }
+  .endborder{
+    border-right: none;
+    border-bottom: lightblue dashed 1px;
+  }
+  .row{
+    display: flex;
+    flex-direction:column;
+  }
 }
 </style>
