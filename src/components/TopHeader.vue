a6b0a607c357393efbb0e7624df15a2abece1ba9
IskahnX <davidecroce10@gmail.com>
2022-02-21 14:18:01 +0100

    top header issue resolved

--- src/components/TopHeader.vue
+++ src/components/TopHeader.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="topheader container">
           <div class="row">
-              <div class="col-6 col-sm-6 col-md-6 d-flex align-items-center">
+              <div class="col-6 col-sm-6 col-md-6 col-lg-3 d-flex align-items-center">
                     <img src="../assets/img/dark-logo.png" alt="">
               </div>
               <div class="col-md-6 col-sm-6 col-6 menu" >
                   <nav>
-                      <ul class="d-flex justify-content-around align-items-center mt-4">
+                      <ul class="d-flex justify-content-around align-items-center mt-4 classic-nav">
                           <li>Home <i class="fas fa-chevron-down"></i></li>
                           <li>Pages <i class="fas fa-chevron-down"></i></li>
                           <li>Courses <i class="fas fa-chevron-down"></i></li>
@@ -14,7 +14,19 @@
                           <li>Blog <i class="fas fa-chevron-down"></i></li>
                           <li>Shop <i class="fas fa-chevron-down"></i></li>
                       </ul>
-                      <i class="fas fa-bars"></i>
+                      <div class="vertical-menu">
+                          <i class="fas fa-bars"></i>
+                          <div class="vertical-nav">
+                                <ul class="d-flex vertical-ul flex-row">
+                                    <li> <i class="fas fa-chevron-down"></i>  Home</li>
+                                    <li> <i class="fas fa-chevron-down"></i>  Pages</li>
+                                    <li> <i class="fas fa-chevron-down"></i>  Courses</li>
+                                    <li> <i class="fas fa-chevron-down"></i>  Features</li>
+                                    <li> <i class="fas fa-chevron-down"></i>  Blog</li>
+                                    <li> <i class="fas fa-chevron-down"></i>  Shop</li>
+                                </ul>
+                          </div>
+                      </div>
                   </nav>
               </div>
               <div class="col-2 d-flex justify-content-around align-items-center ms-5 socials">
@@ -40,7 +52,7 @@ name: "TopHeader",
 .fa-chevron-down{
     font-size: 10px;
 }
-ul{
+.classic-nav{
     list-style-type: none;
     li{
         align-self: center;
@@ -53,6 +65,27 @@ img{
 li:hover, i:hover, img:hover{
     cursor: pointer;
 }
+
+.vertical-ul{
+    position: absolute;
+    // background-color: #fff;
+    right: 5px;
+    list-style-type: none;
+    padding-left: 0;
+    padding: 10px;
+    display: none;
+    li{
+        display: none;
+        padding-left: 20px;
+    }
+}
+
+.vertical-menu:hover .vertical-ul{
+    li{
+        display: block;
+    }
+}
+
 
 @media screen and (max-width: 575px){
     .col-6{
@@ -113,11 +146,13 @@ li:hover, i:hover, img:hover{
             display: none;
         }
     }
-    li{
+    .classic-nav{
+        li{
         padding: 0 12px;
         .fas{
             display:none;
         }
+    }
     }
 }
 </style>
